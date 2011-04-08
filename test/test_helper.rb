c2f8a7f038958652527c0eee95be349a36983d93
Josep M. Bach <josep.m.bach@gmail.com>
2011-04-08 16:47:32 +0200

    Make simplecov work for travis plzzzz

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,12 @@
+require 'rubygems' if RUBY_VERSION == '1.8.7'
+
+require 'bundler/setup'
+
+require 'simplecov'
+SimpleCov.start do
+  add_group "Lib", "lib"
+end
+
 gem 'minitest'
 require 'minitest/unit'
 require 'minitest/autorun'
